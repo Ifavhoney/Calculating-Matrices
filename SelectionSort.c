0f42339a7714105411c10bce9c70f3eae0818be9
Jason <nuthsaid@outlook.com>
2019-08-11 20:28:05 -0400

    Fixed selectionSort method

--- SelectionSort.c
+++ SelectionSort.c
@@ -1,40 +1,46 @@
+
 #include <stdio.h>
-void swapNumber(int arr[], int x, int y);
+void swapNumber(int *x, int *y);
 void selectionSort(int data[], int size);
 
 int main(){
     
-    int arr[5] = {5, 1, 9, 30, 11};
+    int arr[] = {3, 9, 1, 44, 43};
+
     int size = sizeof(arr)/sizeof(arr[0]);
     selectionSort(arr, size);
-
+    
     for (int i = 0; i < size; i++) {
         printf("%d ", arr[i]);
     }
 }
 void selectionSort(int data[], int size){
-    
+    int last_index;
     for (int i = 0; i < size; i++) {
-        //use variable to variable to get index;
-        if (i == 0 ){
-            continue;
+        //always comparing forward
+        //0
+        last_index = i;
+        //0+1
+        for (int j = i + 1; j < size; j++) {
+            //64 > 25 > 12 > 22 > 11
+            if (data[last_index] > data[j]){
+                //get the j
+                last_index = j;
+                //keep iterating
+            }
         }
-        // 64 (i-1) > 25 (i)
-        if (data[i-1] > data[i]){
-            swapNumber(data, i - 1 , i);
-            i = 0;
-        }
-        
+        //passing by reference using array
+        swapNumber(&data[last_index], &data[i]);
     }
-    
-    
 }
 
-void swapNumber(int arr[], int x, int y){
-    //passing by reference
-    int temp = *(arr + x);
-    //64 = 25
-    *( arr+x ) = *(arr+y);
-    //25 = 64
-    *(arr + y) = temp;
+void swapNumber(int *x, int *y){
+  //  passing by reference
+    int temp = *x;
+ //   64 = 11
+    *x = *y;
+ //   11 = 64
+    *y = temp;
 }
+
+
