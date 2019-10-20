d9fdf2ccc78bfc22560e915f89ed6b0ca5782ab3
Ifavhoney <nuthsaid@outlook.com>
2019-10-20 15:33:01 -0400

    Completes the assignment (minus sort)

--- text.h
+++ text.h
@@ -4,6 +4,10 @@
 #include <string.h>
 #include <stdlib.h>
 
+/*
+full name: Jason Eddy N'Guessan, student ID number: 1079936, and uoguelph e-mail: jnguessa@uoguelph.ca
+ */
+
 struct node_struct
 {
     void *data;
@@ -17,7 +21,8 @@ struct node_struct *search(struct node_struct *list, char *target, int (*compar)
 struct node_struct *copy(struct node_struct *start, struct node_struct *end);
 int ftext(FILE *fp, struct node_struct *list);
 struct node_struct *sort(struct node_struct *list, int (*compar)(const void *, const void *));
+
 void remove_repeats(struct node_struct *list, int (*compar)(const void *, const void *));
 int length(struct node_struct *list);
 void free_list(struct node_struct *list, int free_data);
-#endif // text_h
+#endif
