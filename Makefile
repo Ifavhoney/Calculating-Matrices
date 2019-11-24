d5514557776ba3eecb4cd797b1795983fa90f03b
Ifavhoney <nuthsaid@outlook.com>
2019-11-24 15:04:00 -0500

    regrade

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ all:  a4.o
 	gcc -g a4.o -o a4
 
 a4.o: a4.c
-	gcc -Wall -ansi -g -pedantic a4.c -c -o a4.o
+	gcc -Wall -ansi -pedantic a4.c -c -o a4.o
 
 clean:
 	rm *.o
--- a4.h
+++ a4.h
@@ -5,16 +5,18 @@ full name: Jason Eddy N'Guessan, student ID number: 1079936, and uoguelph e-mail
 #define a4_h
 
 #include <stdio.h>
-int hash1(char *s, int max);
 int hash2(char *s, int max);
-int hash3(char *s, int max);
+
 
 int hash1RedMain(char c, int max);
 int hash1Red2(char c, int max);
 int hash1RedNum(char c, int max);
 int hash1Red5(char c, int max);
-int reduce(char c, int max);
+int hash3(char *s, int max);
+int hash1(char *s, int max);
 int reduce2(char c, int max);
 char asciConvert(char *c, int counter);
 long hash(long x);
+
+int reduce(char c, int max);
 #endif
