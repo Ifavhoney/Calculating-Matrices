a8b1f3bfc298891c2f9deade83f41f3a45815f10
Ifavhoney <nuthsaid@outlook.com>
2020-03-03 22:58:13 -0500

    Sorts number & individually sorts array

--- P11.c
+++ P11.c
@@ -1,5 +1,5 @@
 //
-//  P11.c
+//  P21.c
 //  a3490
 //
 //  Created by Jason Eddy on 2020-03-02.
@@ -71,7 +71,7 @@ bool validateAnagram(char *temp){
     
                   //Validate
                        for (int i  = 0; i < strlen(temp); i++) {
-                           printf("%c,", temp[i]);
+                         //  printf("%c,", temp[i]);
                            if(temp[i] != 'X'){
                                isValid = false;
                                break;
@@ -79,104 +79,156 @@ bool validateAnagram(char *temp){
                          }
     return isValid;
 }
+int totalAnagram(char *temp, char* array[]){
+    long tempLength = strlen(temp);
+    int countAnagram = 0;
+    char *clonedTemp = malloc(1000);
+    strcpy(clonedTemp, temp);
+
+      
+     int i = 0;
+
+          int arrayIndex = 0;
+          while(i != tempLength){
+              //invalid
+              if(arrayIndex > 29999){
+                  
+                  break;
+              }
+              else{
+                  //skip those that are not the same length
+                  long tempLengthArray = strlen(array[arrayIndex]);
+                  
+                  if(tempLengthArray != tempLength){
+                         //      printf("HHHHHERE %s, %s\n", temp, array[arrayIndex] );
+                               i = 0;
+                               arrayIndex++;
+                              
+                               continue;
+                           }
+              }
+           
+              //user inputed variable in char
+              char myTempChar = temp[i];
+              int j = 0;
+            //  printf("\ncurrChar: %c\n", myTempChar);
+              while(j != tempLength){
+                  //
+                  char myArrayChar = array[arrayIndex][j];
+                //  printf("%c\t", myArrayChar);
+                  if(myTempChar == myArrayChar){
+                   //   printf("Found!%c @ index: %d \n", myTempChar, j);
+                      temp[i] = 'X';
+                      array[arrayIndex][j] = 'X';
+                      break;
+                  }
+                  j++;
+              }
+              //1802938834
+              i++;
+              if(i == tempLength){
+               bool isValid = validateAnagram(temp);
+                  
+                  if(isValid == true){
+                      countAnagram++;
+                   //   printf("\nvalid anagram: %s @ arrayIndex %d", temp, arrayIndex);
+                  }
+                  if(arrayIndex < 30000){
+                      i = 0;
+                      arrayIndex++;
+                      strcpy(temp, clonedTemp);
+                      continue;
+                  }
+              }
+          
+          //printf("??%s\n", tempArray[sizeof);
+         // printf("\n");
+       
+      }
+       return countAnagram;
+
+
+}
+void swap(int *i, int *i2)
+{
+    int temp = *i;
+    *i = *i2;
+    *i2 = temp;
+}
+long sortOneNumber(int num){
+    int j = 0;
+       int clone = num;
+
+       int length = 0;
+       while(clone != 0){
+           clone = clone /10;
+           length++;
+       }
+       printf("%d, length\n", length);
+       int orderNum[length];
+       for (int i = 0; i < length; i++) {
+           orderNum[i] = num % 10;
+           num = num / 10;
+       }
+    printf("\n\n");
+       for (int i = 0 ; i < length; i++) {
+            int min = i;
+           
+            for (j = i + 1; j < length; j++) {
+                if(orderNum[min] < orderNum[j]){
+                    min = j;
+                }
+            }
+            swap(&orderNum[min], &orderNum[i]);
+        }
+
+ 
+
+    //turn into a number
+    char buff[length + 1];
+    for (int i =0; i<length; i++) {
+        
+           sprintf(&buff[i],"%d",orderNum[i]);
+       }
+    char *str;
+  long  ret = strtol(buff, &str, 10);
+    
+    return ret;
+}
+//length - 1 for end of indices
+
 
 int main(int argc, const char * argv[]) {
     char *fileName = malloc(30);
     strcpy(fileName, "data_4.txt");
-    char *array[30000];
+    char *unsortedarray[30000];
+    char *sortedarray[30000];
+    int intArray[30000];
+    int intArray2[30000];
+    int j = 0;
+    //turns into an int
+    p2InsertArray(fileName, unsortedarray);
+    for (int i = 0; i < 30000; i++) {
+        intArray[i] = atoi(unsortedarray[i]);
+    }
     
-    char *temp = malloc(1000);
-    char *clonedTemp = malloc(1000);
-
-    int countAnagram = 0;
-    p2InsertArray(fileName, array);
-
-    printf("Which string are you looking to anagram  \n");
-    scanf("%s", temp);
-    strcpy(temp, "231106831");
-    strcpy(clonedTemp, temp);
-    int i = 0;
-    //ARRAY: 2898
-    //2889
-printf("18222: %s", array[18222]);
-    long tempLength = strlen(temp);
+  printf("\n%ld\n",sortOneNumber(1804289383));
     
-   
-        int arrayIndex = 0;
-        while(i != tempLength){
-            //invalid
-            if(arrayIndex > 29999){
-                
-                break;
+    for (int i = 0 ; i < 30000; i++) {
+        int min = i;
+        for (j = i + 1; j < 30000; j++) {
+            if(intArray[min] > intArray[j]){
+                min = j;
             }
-            else{
-                //skip those that are not the same length
-                long tempLengthArray = strlen(array[arrayIndex]);
-                
-                if(tempLengthArray != tempLength){
-                             printf("HHHHHERE %s, %s\n", temp, array[arrayIndex] );
-                             i = 0;
-                             arrayIndex++;
-                            
-                             continue;
-                         }
-            }
-         
-            //user inputed variable in char
-            char myTempChar = temp[i];
-            int j = 0;
-            printf("\ncurrChar: %c\n", myTempChar);
-            while(j != tempLength){
-                //
-                char myArrayChar = array[arrayIndex][j];
-                printf("%c\t", myArrayChar);
-                if(myTempChar == myArrayChar){
-                 //   printf("Found!%c @ index: %d \n", myTempChar, j);
-                    temp[i] = 'X';
-                    array[arrayIndex][j] = 'X';
-                    break;
-                }
-                j++;
-            }
-            //1802938834
-            i++;
-            if(i == tempLength){
-             bool isValid = validateAnagram(temp);
-                
-                if(isValid == true){
-                    countAnagram++;
-                    printf("\nvalid anagram: %s @ arrayIndex %d", temp, arrayIndex);
-                }
-                if(arrayIndex < 30000){
-                    i = 0;
-                    arrayIndex++;
-                    strcpy(temp, clonedTemp);
-                    continue;
-                }
-            }
-        
-        //printf("??%s\n", tempArray[sizeof);
-        printf("\n");
-     
+        }
+        swap(&intArray[min], &intArray[i]);
     }
-     
-    printf("\ntotal anagram: %d\n",countAnagram );
-  p2InsertArray(fileName, array);
-    /*
-    i = 0;
-    free(temp);
-while (i <= sizeof(array)/sizeof(array[0])){
-    //empty
-    if(strlen(array[i]) < 3 || strstr(array[i], " ") != NULL){
-break;
-    }
-    else{
-           free(array[i]);
-    }
-    i++;
-  
-}
-  */
+    //Re-sort
+    
+
+    
+    
+    
     return 0;
+ 
 }
-
